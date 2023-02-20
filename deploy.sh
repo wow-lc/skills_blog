36ed5e2ce3282366c50278247ba8c90d106519a9
lichao <lichao@hzwotu.com>
2023-02-20 16:21:44 +0800

    chore

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wow-lc/skills_blog.git master:gh-pages
+git push -f git@github.com:wow-lc/skills_blog.git main:gh-pages
 
 cd -
